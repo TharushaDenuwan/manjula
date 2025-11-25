ea97fc40e218d25ccb38d646926967448e28bfe0
TharushaDenuwan <tharushadenuwan35@gmail.com>
2025-11-25 19:13:34 +0530

    updated codes

--- apps/web/src/app/account/@hotel/setup/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SetupHotel } from "@/features/hotels/components/setup-hotel";
-
-type Props = {};
-
-export default function HotelSetupPage({}: Props) {
-  return <SetupHotel className="mt-8 shadow-none" />;
-}
