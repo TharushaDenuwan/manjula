13bad5e4fa58e39ea036a1dbd6d4f6e4aa226fbf
TharushaDenuwan <tharushadenuwan35@gmail.com>
2026-01-02 16:09:11 +0530

    update changes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,18 +1,37 @@
+// import createApp from "@api/lib/create-app";
+// import configureOpenAPI from "@api/lib/open-api-config";
+// import { registerRoutes } from "@api/routes/registry";
+
+// import env from "./env";
+
+// const app = registerRoutes(createApp());
+
+// configureOpenAPI(app);
+
+// export type AppType = typeof app;
+
+// export default {
+//   port: env.PORT,
+//   fetch: app.fetch
+// };
+
+// // Test
+
 import createApp from "@api/lib/create-app";
 import configureOpenAPI from "@api/lib/open-api-config";
 import { registerRoutes } from "@api/routes/registry";
-
 import env from "./env";
 
 const app = registerRoutes(createApp());
-
 configureOpenAPI(app);
 
-export type AppType = typeof app;
+// Start Bun server
+if (import.meta.main) {
+  app.fire({ port: env.PORT || 5000 });
+}
 
+export type AppType = typeof app;
 export default {
   port: env.PORT,
-  fetch: app.fetch
+  fetch: app.fetch,
 };
-
-// Test
