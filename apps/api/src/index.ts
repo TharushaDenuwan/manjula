c540faa9ccd4d96bd03888432cc28853a9453db1
TharushaDenuwan <tharushadenuwan35@gmail.com>
2026-01-02 17:32:38 +0530

    update changes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,36 +1,33 @@
-import createApp from "@api/lib/create-app";
-import configureOpenAPI from "@api/lib/open-api-config";
-import { registerRoutes } from "@api/routes/registry";
-
-import env from "./env";
-
-const app = registerRoutes(createApp());
-
-configureOpenAPI(app);
-
-export type AppType = typeof app;
-
-export default {
-  port: env.PORT,
-  fetch: app.fetch,
-};
-
-// // Test
-
 // import createApp from "@api/lib/create-app";
 // import configureOpenAPI from "@api/lib/open-api-config";
 // import { registerRoutes } from "@api/routes/registry";
+
 // import env from "./env";
 
 // const app = registerRoutes(createApp());
+
 // configureOpenAPI(app);
 
-// const port = env.PORT;
+// export type AppType = typeof app;
 
-// // Bun-compatible server
-// Bun.serve({
-//   port,
-//   fetch: app.fetch, // Bun uses fetch handler
-// });
+// export default {
+//   port: env.PORT,
+//   fetch: app.fetch,
+// };
 
-// console.log(`🚀 API running on port ${port}`);
+// // Test
+
+import createApp from "@api/lib/create-app";
+import configureOpenAPI from "@api/lib/open-api-config";
+import { registerRoutes } from "@api/routes/registry";
+import env from "./env";
+
+const app = registerRoutes(createApp());
+configureOpenAPI(app);
+
+Bun.serve({
+  port: env.PORT,
+  fetch: app.fetch,
+});
+
+console.log(`🚀 API running on port ${env.PORT}`);
