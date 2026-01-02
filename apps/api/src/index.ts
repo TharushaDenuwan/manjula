91b7529bf27659c16b6e88594c369811e5c18a4b
TharushaDenuwan <tharushadenuwan35@gmail.com>
2026-01-02 16:39:28 +0530

    update changes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,18 +1,32 @@
+// import createApp from "@api/lib/create-app";
+// import configureOpenAPI from "@api/lib/open-api-config";
+// import { registerRoutes } from "@api/routes/registry";
+
+// import env from "./env";
+
+// const app = registerRoutes(createApp());
+
+// configureOpenAPI(app);
+
+// export type AppType = typeof app;
+
+// export default {
+//   port: env.PORT,
+//   fetch: app.fetch,
+// };
+
+// // Test
+
 import createApp from "@api/lib/create-app";
 import configureOpenAPI from "@api/lib/open-api-config";
 import { registerRoutes } from "@api/routes/registry";
-
 import env from "./env";
 
 const app = registerRoutes(createApp());
-
 configureOpenAPI(app);
 
-export type AppType = typeof app;
+const port = parseInt(env.PORT || "5000", 10);
 
-export default {
-  port: env.PORT,
-  fetch: app.fetch,
-};
-
-// Test
+app.listen(port, () => {
+  console.log(`🚀 API running on port ${port}`);
+});
