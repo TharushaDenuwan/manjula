a84796ad623d933d01a6f61849059b408a259a5c
TharushaDenuwan <tharushadenuwan35@gmail.com>
2026-01-02 17:07:50 +0530

    updated changes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -17,20 +17,37 @@
 
 // // Test
 
+// import createApp from "@api/lib/create-app";
+// import configureOpenAPI from "@api/lib/open-api-config";
+// import { registerRoutes } from "@api/routes/registry";
+// import env from "./env";
+
+// const app = registerRoutes(createApp());
+// configureOpenAPI(app);
+
+// const port = env.PORT;
+
+// // Bun-compatible server
+// Bun.serve({
+//   port,
+//   fetch: app.fetch, // Bun uses fetch handler
+// });
+
+// console.log(`🚀 API running on port ${port}`);
+
 import createApp from "@api/lib/create-app";
 import configureOpenAPI from "@api/lib/open-api-config";
 import { registerRoutes } from "@api/routes/registry";
+
 import env from "./env";
 
 const app = registerRoutes(createApp());
+
 configureOpenAPI(app);
 
-const port = env.PORT;
+export type AppType = typeof app;
 
-// Bun-compatible server
-Bun.serve({
-  port,
-  fetch: app.fetch, // This is why you exported `fetch`
-});
-
-console.log(`🚀 API running on port ${port}`);
+export default {
+  port: env.PORT,
+  fetch: app.fetch,
+};
