6358967c8d6069a2d56c929fc934d00e2bc17729
TharushaDenuwan <tharushadenuwan35@gmail.com>
2026-01-02 17:52:31 +0530

    hhh

--- apps/api/src/routes/auth/auth.config.ts
+++ apps/api/src/routes/auth/auth.config.ts
@@ -1,32 +1,11 @@
-import { db } from "@api/db";
-import env from "@api/env";
 import { betterAuth } from "better-auth";
 
 export const auth = betterAuth({
-  database: db,
-  baseURL: env.BETTER_AUTH_URL,
-  trustedOrigins: [env.CLIENT_APP_URL],
-
-  session: {
-    cookieCache: {
-      enabled: true,
-      maxAge: 5 * 60, // 5 minutes
-    },
-  },
-
   user: {
     changeEmail: {
       enabled: true,
     },
     deleteUser: {
-      enabled: true,
-    },
-  },
-
-  advanced: {
-    cookiePrefix: "manjula",
-    useSecureCookies: env.NODE_ENV === "production",
-    crossSubDomainCookies: {
       enabled: true,
     },
   },
